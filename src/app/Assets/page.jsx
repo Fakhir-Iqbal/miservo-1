2d6fff6444aa462c522b4dbe80ad3a4d4c4de7ab
Fakhir-Iqbal <fakhiriqbal>
2024-12-16 21:56:44 +0500

    Fix Assets Api Issue ...

--- src/app/Assets/page.jsx
+++ src/app/Assets/page.jsx
@@ -1,175 +1,3 @@
-// "use client";
-
-// import React from "react";
-// import Icon from "@root/src/components/Icon";
-// import { addCircle, back, pencil } from "@root/icons";
-// import Link from "next/link";
-// import Navbar from "@root/src/components/Navbar/page";
-// import Attachments from "@root/src/components/AddAttachments/page";
-
-// const beneficiaries = [
-//   {
-//     index: 1,
-//     name: "John Doe",
-//   },
-//   {
-//     index: 2,
-//     name: "Jane Smith",
-//   },
-//   {
-//     index: 3,
-//     name: "Alice Johnson",
-//   },
-//   {
-//     index: 2,
-//     name: "Jane Smith",
-//   },
-// ];
-// const beneficiaryDetails = [
-//   {
-//     label: "Title",
-//     value: "Personal Property",
-//   },
-//   {
-//     label: "Beneficiary",
-//     value: "Doe",
-//   },
-//   {
-//     label: "Description",
-//     value:
-//       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with",
-//   },
-// ];
-// const personalDetails = [
-//   {
-//     label: "First Name",
-//     value: "John",
-//   },
-//   {
-//     label: "Last Name",
-//     value: "Doe",
-//   },
-//   {
-//     label: "Relation",
-//     value: "Son",
-//   },
-//   {
-//     label: "Phone",
-//     value: "+123-456-789",
-//   },
-//   {
-//     label: "Phone#2",
-//     value: "+123-456-789",
-//   },
-//   {
-//     label: "Whatsapp",
-//     value: "+123-456-789",
-//   },
-//   {
-//     label: "Email",
-//     value: "franciscojavier@gmail.com",
-//   },
-//   {
-//     label: "Another Email",
-//     value: "-",
-//   },
-//   {
-//     label: "Address",
-//     value:
-//       "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-//   },
-//   {
-//     label: "Address#2",
-//     value: "Same as above",
-//   },
-//   {
-//     label: "Assigned Assets",
-//     value: "John Xavier",
-//   },
-// ];
-
-// const Assests = () => {
-//   return (
-//     <>
-//       <Navbar />
-//       <div className="p-6">
-//         <div className="flex justify-between">
-//           <h1 className="text-3xl font-bold">Assets</h1>
-//           <div className="flex items-center">
-//             <Link
-//               href="/AddAssets"
-//               className="flex gap-2 font-semibold underline decoration-yellow-800 text-sm decoration-2"
-//             >
-//               {" "}
-//               ADD ASSETS <Icon src={addCircle} className="w-4" />
-//             </Link>
-//             <span className="bg-white mx-3 p-3 rounded-full">
-//               {" "}
-//               <Icon src={back} className="w-3 h-3" />
-//             </span>
-//             <span className="bg-white p-3 rounded-full">
-//               {" "}
-//               <Icon src={back} className="w-3 h-3 rotate-180" />
-//             </span>
-//           </div>
-//         </div>
-//         <div className="flex gap-5 py-6">
-//           {beneficiaries.map((data) => (
-//             <div className={`p-4 py-7 rounded-xl w-full bg-[#FFF9EB]`}>
-//               <h1 className="font-extrabold text-lg mb-2">
-//                 #{data.index ?? "1"}
-//               </h1>
-//               <div className="grid gap-2">
-//                 <div className="text-lg font-bold ">
-//                   {data.name ?? "Francisco Javier"}
-//                 </div>
-//               </div>
-//             </div>
-//           ))}
-//         </div>
-
-//         <div className="bg-white my-7 p-4 rounded-2xl">
-//           <div className="flex justify-between py-3">
-//             <h2 className="text-3xl font-bold text-blue-900 ">
-//               Personal Details
-//             </h2>
-//             <Link href="/" className="flex gap-2 py-3">
-//               Edit Details <Icon src={pencil} className="w-4" />
-//             </Link>
-//           </div>
-
-//           <div className="">
-//             {beneficiaryDetails.map((detail, index) => (
-//               <p key={index} className="pb-5">
-//                 <span className="font-bold">{detail.label}:</span>{" "}
-//                 {detail.value}
-//               </p>
-//             ))}
-//           </div>
-
-//           <div className="py-7">
-//             <p className="font-bold mb-3">Attach Document/Images/Videos:</p>
-//             <Attachments />
-//           </div>
-
-//           <div className="grid grid-cols-1 md:grid-cols-3 gap-4 ">
-//             {personalDetails.map((detail, index) => (
-//               <p key={index}>
-//                 <span className="font-bold">{detail.label}:</span>{" "}
-//                 {detail.value}
-//               </p>
-//             ))}
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Assests;
-
-
-
 "use client";
 
 import React, { useState, useEffect } from "react";
@@ -182,20 +10,18 @@ import axios from "axios";
 
 const Assests = () => {
   const [data, setData] = useState([]); // Initialize as an empty array
-  const [activeAssests, setActiveAssests] = useState(null); // Will hold the currently selected Assests
   const [currentIndex, setCurrentIndex] = useState(0);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   const savedData = localStorage.getItem("loginData");
-  let converSaveData = JSON.parse(savedData);
-  let token = converSaveData?.data?.token;
+  const token = savedData ? JSON.parse(savedData)?.data?.token : null;
 
   useEffect(() => {
     const fetchAssests = async () => {
       try {
         const response = await axios.get(
-          "https://miservo-api.vercel.app/api/Attorney/get",
+          "https://miservo-api.vercel.app/api/asset/get",
           {
             headers: {
               "Content-Type": "application/json",
@@ -205,36 +31,33 @@ const Assests = () => {
         );
         if (response.status === 200) {
           setData(response.data.data);
-          setActiveAssests(response.data.data[0]);
           setLoading(false);
         }
       } catch (error) {
-        setError("No Assests Found");
+        setError("No Assets Found");
         setLoading(false);
-
-
       }
     };
 
-    fetchAssests();
+    if (token) {
+      fetchAssests();
+    }
   }, [token]);
 
   const nextAssests = () => {
-    const nextIndex = (currentIndex + 1) % data.length;
-    setCurrentIndex(nextIndex);
-    setActiveAssests(data[nextIndex]);
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % data.length);
   };
 
   const prevAssests = () => {
-    const prevIndex = (currentIndex - 1 + data.length) % data.length;
-    setCurrentIndex(prevIndex);
-    setActiveAssests(data[prevIndex]);
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + data.length) % data.length);
   };
+
+  const activeAssests = data[currentIndex];
 
   if (loading) {
     return (
       <div className="flex justify-center items-center h-screen">
-        <p>Loading...</p>
+        <p>Loading assets, please wait...</p>
       </div>
     );
   }
@@ -244,7 +67,7 @@ const Assests = () => {
       <div className="flex justify-center items-center h-screen">
         <div className="text-center">
           <p className="text-2xl font-bold mb-4">{error}</p>
-          <p className="text-gray-500">Please add Assests to view them here.</p>
+          <p className="text-gray-500">Please add assets to view them here or check your internet connection.</p>
         </div>
       </div>
     );
@@ -255,13 +78,13 @@ const Assests = () => {
       <Navbar />
       <div className="p-6">
         <div className="flex justify-between">
-          <h1 className="text-3xl font-bold">Assests</h1>
+          <h1 className="text-3xl font-bold">Assets</h1>
           <div className="flex items-center">
             <Link
               href="/AddAssets"
               className="flex gap-2 font-semibold underline decoration-yellow-800 text-sm decoration-2"
             >
-              ADD ASSESTS <Icon src={addCircle} className="w-4" />
+              ADD ASSETS <Icon src={addCircle} className="w-4" />
             </Link>
             <span
               className="bg-white mx-3 p-3 rounded-full cursor-pointer"
@@ -280,107 +103,39 @@ const Assests = () => {
 
         {/* Slider for cards */}
         <div className="flex gap-5 py-6 overflow-x-auto">
-          {data?.slice(0, 5).map((Assests, index) => (
+          {data?.slice(0, 5).map((asset, index) => (
             <BeneficiariesCard
-              key={Assests._id}
+              key={asset._id}
               index={index + 1}
-              name={`${Assests.firstName} ${Assests.lastName}`}
-              email={Assests.email}
-              phone={Assests.contactNumber}
-              relation={Assests.relationShip}
-              bg={false}
-              onClick={() => {
-                setActiveAssests(Assests);
-                setCurrentIndex(index);
-              }}
+              name={asset.title}
+              email={asset.email}
+              phone={asset.contactNumber}
+              location={asset.firstAddress}
+              assests={asset}
+              image={asset.assetPicture}
             />
           ))}
         </div>
 
-        {/* Full details section */}
-        {activeAssests && (
-          <div className="bg-white my-7 p-4 rounded-2xl">
-            <div className="flex justify-between py-3">
-              <h2 className="text-3xl font-bold">
-                {currentIndex + 1}. {`${activeAssests.firstName} ${activeAssests.lastName}`}
-              </h2>
-              <Link href="/" className="flex gap-2 py-3">
-                Edit Details <Icon src={pencil} className="w-4" />
-              </Link>
+        <div className="mt-6">
+          <h2 className="text-2xl font-bold mb-4">Asset Details</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div>
+              <p><span className="font-bold">Title:</span> {activeAssests?.title}</p>
+              <p><span className="font-bold">Asset Number:</span> {activeAssests?.assetNumber}</p>
+              <p><span className="font-bold">Documents/Video/Image:</span> {activeAssests.documents.length > 0 ? <img src={activeAssests.documents[0].url} className="w-20 h-24" /> : 'No document available'}</p>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-
-
-              <p className="col-span-3" >
-                <span className="font-bold">Title:</span> {activeAssests.lastName}
-              </p>
-              <p className="col-span-3" >
-                <span className="font-bold">Beneficiary:</span> {activeAssests.lastName}
-              </p>
-              <p className="col-span-3" >
-                <span className="font-bold">Description:</span> {activeAssests.lastName}
-              </p>
-              <p className="col-span-3" >
-                <span className="font-bold">Document/Video/Image:</span> <br />
-                <label className="border-2 border-dashed border-gray-400 w-20 h-24 flex items-center justify-center cursor-pointer">
-                  Img
-                </label>
-              </p>
-
-
-
-
-
-
-
-
-
-
-
-              <p>
-                <span className="font-bold">First Name:</span>{" "}
-                {activeAssests.firstName}
-              </p>
-              <p>
-                <span className="font-bold">Last Name:</span> {activeAssests.lastName}
-              </p>
-              <p>
-                <span className="font-bold">Relation:</span> {activeAssests.relationShip}
-              </p>
-              <p>
-                <span className="font-bold">Phone:</span> {activeAssests.contactNumber}
-              </p>
-              <p>
-                <span className="font-bold">Phone#2:</span> {activeAssests.whatsappNumber}
-              </p>
-              <p>
-                <span className="font-bold">Whatsapp:</span> {activeAssests.whatsappNumber}
-              </p>
-              <p>
-                <span className="font-bold">Email:</span> {activeAssests.email}
-              </p>
-              <p>
-                <span className="font-bold">Another Email:</span>{" "}
-                {activeAssests.anotherEmail}
-              </p>
-              <p>
-                <span className="font-bold">Address:</span> {activeAssests.firstAddress}
-              </p>
-              <p>
-                <span className="font-bold">Address#2:</span> {activeAssests.secondAddress}
-              </p>
-              <p>
-                <span className="font-bold">Assigned Assets:</span>{" "}
-                {activeAssests.assetsAssigned}
-              </p>
+            <div>
+              <p><span className="font-bold">Beneficiary Name:</span> {activeAssests.beneficiaryDetails?.firstName} {activeAssests.beneficiaryDetails?.lastName}</p>
+              <p><span className="font-bold">Phone:</span> {activeAssests.beneficiaryDetails?.contactNumber}</p>
+              <p><span className="font-bold">Email:</span> {activeAssests.beneficiaryDetails?.email}</p>
             </div>
           </div>
-        )}
+        </div>
       </div>
     </>
   );
 };
 
 export default Assests;
-
